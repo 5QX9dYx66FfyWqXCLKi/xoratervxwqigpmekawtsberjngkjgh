b99d0c175ddf6264bd90c8655efe7e32f83dc1ae
helowoi <32401742+helowoi@users.noreply.github.com>
2019-03-16 09:00:54 +0700

    x

---
+++ radio/station/2.js
@@ -0,0 +1,95 @@
+<script>
+info="<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset=&#34;utf-8&#34;>
+  <meta name=&#34;viewport&#34; content=&#34;width=device-width, initial-scale=1&#34;>
+  <link rel=&#34;stylesheet&#34; href=&#34;https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css&#34; type=&#34;text/css&#34;>
+  <link rel=&#34;stylesheet&#34; href=&#34;theme.css&#34; type=&#34;text/css&#34;>
+</head>
+
+<body>
+  <div class=&#34;py-3&#34; style=&#34;&#34;>
+    <div class=&#34;container&#34;>
+      <div class=&#34;row&#34;>
+        <a class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34; href=&#34;../prambors.html&#34;><img src=&#34;aset/prambors.jpg&#34; alt=&#34;Radio Online AGM&#34; class=&#34;img-fluid&#34;></a>
+        <a class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34; href=&#34;cinde-fm.html&#34;><img src=&#34;aset/cindefm.png&#34; alt=&#34;Radio Online AGM&#34; class=&#34;img-fluid&#34;></a>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/cindefm.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/kc10.jpg&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/bens.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+      </div>
+      <div class=&#34;row&#34;>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+      </div>
+      <div class=&#34;row&#34;>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+      </div>
+      <div class=&#34;row&#34;>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+      </div>
+      <div class=&#34;row&#34;>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+        <div class=&#34;col-md-1 col-3 my-1&#34; style=&#34;&#34;><img class=&#34;img-fluid d-block&#34; src=&#34;aset/radio-online-agm-1.png&#34;></div>
+      </div>
+    </div>
+  </div>
+  <script src=&#34;https://code.jquery.com/jquery-3.3.1.slim.min.js&#34; integrity=&#34;sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo&#34; crossorigin=&#34;anonymous&#34;></script>
+  <script src=&#34;https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js&#34; integrity=&#34;sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49&#34; crossorigin=&#34;anonymous&#34;></script>
+  <script src=&#34;https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js&#34; integrity=&#34;sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k&#34; crossorigin=&#34;anonymous&#34;></script>
+</body>
+
+</html>"
+
+document.write(info)
+</script>
