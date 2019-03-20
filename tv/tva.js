44b852bf58aa81b79f2bc10ac50aad8d50510dbe
helowoi <32401742+helowoi@users.noreply.github.com>
2019-03-20 18:26:59 +0700

    x

--- tv/tva.js
+++ tv/tva.js
@@ -2,7 +2,7 @@ document.write( '<!-- CSS  -->\n' );
 document.write( ' <link href=\"https://vjs.zencdn.net/7.2.3/video-js.css\" rel=\"stylesheet\">\n' );
 document.write( '<!-- HTML -->\n' );
 document.write( '<video id=\'hls-example\'  class=\"video-js vjs-default-skin\" width=\"400\" height=\"300\" controls>\n' );
-document.write( '<source type=\"application/x-mpegURL\" src=\"http://www.streambox.fr/playlists/test_001/stream.m3u8\">\n' );
+document.write( '<source type=\"application/x-mpegURL\" src=\"http://moiptvhls-i.akamaihd.net/hls/live/652314/HQs/chunklist.m3u8\">\n' );
 document.write( '</video>\n' );
 document.write( '<!-- JS code -->\n' );
 document.write( '<!-- If you\'d like to support IE8 (for Video.js versions prior to v7) -->\n' );
