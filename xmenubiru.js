d1f7baec5e77f75f86b2e210726d2afb34d4a7a6
helowoi <32401742+helowoi@users.noreply.github.com>
2019-08-19 01:07:31 +0700

    x

--- xmenubiru.js
+++ xmenubiru.js
@@ -9,7 +9,7 @@ document.writeln("           </div> ");
 document.writeln("           <div class=\"col-md-6\"> "); 
 document.writeln("             <a class=\"btn btn-facebook btn-block m-2\" href=\"http://aksesglobalmultimedia.com/seting-telegram.html\">Cara Seting Telegram</a> "); 
 document.writeln("             <a class=\"btn btn-facebook btn-block m-2\" href=\"http://aksesglobalmultimedia.com/cara-seting-gtalk.html\">Cara Seting GTalk / Hangout</a> "); 
-document.writeln("             <a class=\"btn btn-facebook btn-block m-2\" target=\"_blank\" href=\"http://aksesglobalmultimedia.com/cara-seting-jabber.html\">Cara Seting Jabber</a> "); 
+document.writeln("             <a class=\"btn btn-facebook btn-block m-2\" href=\"http://aksesglobalmultimedia.com/cara-seting-jabber.html\">Cara Seting Jabber</a> "); 
 document.writeln("           </div> "); 
 document.writeln("         </div> "); 
 document.writeln("       </div> "); 
