76f4542c3dc8b5aacd389973294c8629078fb31f
5QX9dYx66FfyWqXCLKi <muhamadkhadafi666@gmail.com>
2020-10-24 00:28:56 +0700

    x

--- dpmptsp/jqueryz.js
+++ dpmptsp/jqueryz.js
@@ -1,9 +1,9 @@
-document.writeln("<div class=/"footer-copyright/">");
-document.writeln("      <div class=/"container py-2/">");
-document.writeln("        <div class=/"row py-4/">");
-document.writeln("          <div class=/"col-lg-12 d-flex align-items-center justify-content-center/">");
-document.writeln("            <h5>DPMPTSP Kabupaten Indramayu ©Copyright 2020&nbsp;&nbsp;&nbsp;<a href=/"https://aksesglobalmultimedia.com/" target=/"_blank/">All Rights Reserved&nbsp;</a></h5>");
-document.writeln("          </div>");
-document.writeln("        </div>");
-document.writeln("      </div>");
-document.writeln("    </div>");
+document.write( '<div class=\"footer-copyright\">\n' );
+document.write( '      <div class=\"container py-2\">\n' );
+document.write( '        <div class=\"row py-4\">\n' );
+document.write( '          <div class=\"col-lg-12 d-flex align-items-center justify-content-center\">\n' );
+document.write( '            <h5>DPMPTSP Kabupaten Indramayu ©Copyright 2020   <a href=\"https://aksesglobalmultimedia.com\" target=\"_blank\">All Rights Reserved </a></h5>\n' );
+document.write( '          </div>\n' );
+document.write( '        </div>\n' );
+document.write( '      </div>\n' );
+document.write( '    </div>' );
