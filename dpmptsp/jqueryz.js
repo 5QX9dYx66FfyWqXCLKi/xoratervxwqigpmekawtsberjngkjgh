9c07d964d894caeafdebc09a3a28b0b664560c06
5QX9dYx66FfyWqXCLKi <muhamadkhadafi666@gmail.com>
2020-10-24 00:06:12 +0700

    x

---
+++ dpmptsp/jqueryz.js
@@ -0,0 +1,9 @@
+document.writeln("<div class=/"footer-copyright/">");
+document.writeln("      <div class=/"container py-2/">");
+document.writeln("        <div class=/"row py-4/">");
+document.writeln("          <div class=/"col-lg-12 d-flex align-items-center justify-content-center/">");
+document.writeln("            <h5>DPMPTSP Kabupaten Indramayu ©Copyright 2020&nbsp;&nbsp;&nbsp;<a href=/"https://aksesglobalmultimedia.com/" target=/"_blank/">All Rights Reserved&nbsp;</a></h5>");
+document.writeln("          </div>");
+document.writeln("        </div>");
+document.writeln("      </div>");
+document.writeln("    </div>");
